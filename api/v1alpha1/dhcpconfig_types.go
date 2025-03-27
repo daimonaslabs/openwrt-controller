a4e046e626e5b9f30e1c144520275c67568b1baa
jrodonnell <james.riley.odonnell@gmail.com>
2025-03-27 01:38:25 -0600

    move wrapper_types to go-ubus-rpc

--- api/v1alpha1/dhcpconfig_types.go
+++ api/v1alpha1/dhcpconfig_types.go
@@ -100,7 +100,7 @@ type DnsmasqSection struct {
 	Authoritative ubus.UbusBool `json:"authoritative,omitempty" ubus:"authoritative,omitempty"`
 	// IP addresses to convert into NXDOMAIN responses (to counteract “helpful” upstream DNS servers that never
 	// return NXDOMAIN).
-	BogusNXDOMAIN []Addr `json:"bogusNXDOMAIN,omitempty" ubus:"bogusnxdomain,omitempty"`
+	BogusNXDOMAIN []ubus.IP `json:"bogusNXDOMAIN,omitempty" ubus:"bogusnxdomain,omitempty"`
 	// Reject reverse lookups to private IP ranges where no corresponding entry exists in /etc/hosts.
 	BogusPriv ubus.UbusBool `json:"bogusPriv,omitempty" ubus:"boguspriv,omitempty"`
 	// When set to 0, use each network interface's DNS address in the local /etc/resolv.conf. Normally, only
@@ -161,7 +161,7 @@ type DnsmasqSection struct {
 	// Store DHCP leases in this file.
 	LeaseFile string `json:"leaseFile,omitempty" ubus:"leasefile,omitempty"`
 	// Listen only on the specified IP addresses. If unspecified, listen on IP addresses from each interface.
-	ListenAddress []Addr `json:"listenAddress,omitempty" ubus:"listen_address,omitempty"`
+	ListenAddress []ubus.IP `json:"listenAddress,omitempty" ubus:"listen_address,omitempty"`
 	// Look up DNS entries for this domain from /etc/hosts. This follows the same syntax as Server entries.
 	// See the dnsmasq man page for more details.
 	Local string `json:"local,omitempty" ubus:"local,omitempty"`
